c54a45c22025f13067dfe0bb1765e914d56703c4
Igor <trololol-lolo@mail.ru>
2023-06-09 15:26:43 +1000

    Добавляет активные состояния

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,7 @@ const navToggle = document.querySelector('.main-nav__toggle');
 mainNav.classList.remove('main-nav--no-js');
 mainNav.classList.add('main-nav--closed');
 
-addEventListener('click', () => {
+navToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('main-nav--closed');
   mainNav.classList.toggle('main-nav--opened');
 } );
